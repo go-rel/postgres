c60a4d58ea9dfc942d001899393c7c208cbb1b5c
Fs02 <surya.asriadie@gmail.com>
2021-10-28 02:20:59 +0900

    fix coverage

--- postgres_test.go
+++ postgres_test.go
@@ -2,6 +2,7 @@ package postgres
 
 import (
 	"context"
+	"errors"
 	"os"
 	"testing"
 	"time"
@@ -149,4 +150,10 @@ func TestAdapter_Exec_error(t *testing.T) {
 
 	_, _, err = adapter.Exec(ctx, "error", nil)
 	assert.NotNil(t, err)
+}
+
+func TestCheck(t *testing.T) {
+	assert.Panics(t, func() {
+		check(errors.New("error"))
+	})
 }
